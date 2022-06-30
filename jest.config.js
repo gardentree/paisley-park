4c19f2537a79554110ad11202bafcfb27931bc5c
(ю:】 <gardentreex@gmail.com>
2022-06-30 21:04:28 +0900

    :muscle: Setup jest-environment-jsdom-global

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ const customJestConfig = {
     "^@/components/(.*)$": "<rootDir>/components/$1",
     "^@/pages/(.*)$": "<rootDir>/pages/$1",
   },
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jest-environment-jsdom-global",
 };
 
 module.exports = createJestConfig(customJestConfig);
