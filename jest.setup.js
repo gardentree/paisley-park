9701b66252cee9ad08bbfb0210625b0c6d84680f
(ю:】 <gardentreex@gmail.com>
2022-06-30 12:59:37 +0900

    :muscle: Setup jest

---
+++ jest.setup.js
@@ -0,0 +1,6 @@
+if (typeof global.TextEncoder === "undefined") {
+  global.TextEncoder = require("util").TextEncoder;
+}
+if (typeof global.TextDecoder === "undefined") {
+  global.TextDecoder = require("util").TextDecoder;
+}
