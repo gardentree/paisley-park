08561af48daa3fffd4ea5abbd38ced77fcc50117
(ю:】 <gardentreex@gmail.com>
2022-07-10 16:28:58 +0900

    :shirt: Clean

--- __tests__/pages/api/books/[url].test.ts
+++ __tests__/pages/api/books/[url].test.ts
@@ -1,4 +1,3 @@
-import {render, screen} from "@testing-library/react";
 import {testApiHandler} from "next-test-api-route-handler";
 import handler, {crawlBooks, extractMagazine, crawlPagination} from "@/pages/api/books/[url]";
 
