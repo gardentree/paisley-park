27011239fd30798f2c1438b4c201f7bc2bd738a4
(ю:】 <gardentreex@gmail.com>
2022-07-11 16:18:47 +0900

    :sparkles: Make to exclude magazines

--- components/Bookshelf.tsx
+++ components/Bookshelf.tsx
@@ -1,10 +1,59 @@
 import type React from "react";
-import {Container, Card, Row, Col, Spinner} from "react-bootstrap";
+import {Container, Row, Col, Spinner, Accordion} from "react-bootstrap";
 import Book from "./Book";
+import {useSetWithLocalStorage} from "@/hooks/LocalStorage";
 import styles from "@/styles/Bookshelf.module.css";
 
 interface Props {
   books: Map<string, Book>;
+}
+
+export default function Bookshelf(props: Props) {
+  const {books: books} = props;
+  const [exclusions, toggleExclustion] = useSetWithLocalStorage<string>("exclusions");
+
+  if (books.size <= 0) {
+    return (
+      <div className={styles.spinner}>
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  }
+
+  const entries = Array.from(groupByMagazine(books).entries());
+
+  const action = (event: React.MouseEvent) => {
+    const magazine = event.currentTarget.closest(".accordion-header")!.textContent!;
+
+    toggleExclustion(magazine);
+  };
+
+  return (
+    <Container fluid="md">
+      <div className={styles.list}>
+        {entries.map(([name, magazine]) => {
+          return (
+            <Accordion defaultActiveKey={exclusions.has(name) ? null : "0"} flush className={styles.magazine} key={name}>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header onClick={action}>{name}</Accordion.Header>
+                <Accordion.Body>
+                  <Row className={styles.books}>
+                    {Array.from(magazine.values()).map((book) => {
+                      return (
+                        <Col className={styles.book} md={2} key={book.title}>
+                          <Book attributes={book} />
+                        </Col>
+                      );
+                    })}
+                  </Row>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          );
+        })}
+      </div>
+    </Container>
+  );
 }
 
 function groupByMagazine(books: Map<string, Book>): Map<string, Map<string, Book>> {
@@ -25,43 +74,4 @@ function groupByMagazine(books: Map<string, Book>): Map<string, Map<string, Book
   });
 
   return magazines;
-}
-
-export default function Bookshelf(props: Props) {
-  const {books: books} = props;
-
-  if (books.size <= 0) {
-    return (
-      <div className={styles.spinner}>
-        <Spinner animation="border" variant="primary" />
-      </div>
-    );
-  }
-
-  const entries = Array.from(groupByMagazine(books).entries());
-
-  return (
-    <Container fluid="md">
-      <div className={styles.list}>
-        {entries.map(([name, magazine]) => {
-          return (
-            <Card className={styles.magazine} key={name}>
-              <Card.Header>{name}</Card.Header>
-              <Card.Body>
-                <Row className={styles.books}>
-                  {Array.from(magazine.values()).map((book) => {
-                    return (
-                      <Col className={styles.book} md={2} key={book.title}>
-                        <Book attributes={book} />
-                      </Col>
-                    );
-                  })}
-                </Row>
-              </Card.Body>
-            </Card>
-          );
-        })}
-      </div>
-    </Container>
-  );
 }
