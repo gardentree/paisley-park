38143ef0e7e247904e205f509ed16289e23e29cf
(ю:】 <gardentreex@gmail.com>
2022-07-13 13:04:41 +0900

    :green_heart: Fix test

--- libraries/utility.ts
+++ libraries/utility.ts
@@ -1,10 +1,12 @@
+import * as self from "./utility";
+
 export async function fetchWithRetry(url: URL | string, retry: number): Promise<Response> {
   const response = await fetch(url);
   if (![503].includes(response.status) || retry <= 0) {
     return response;
   }
 
-  await sleep((30 / retry) * 1000);
+  await self.sleep((30 / retry) * 1000);
 
   console.info(`retry[${retry}] ${response.status}: ${url}`);
   return fetchWithRetry(url, --retry);
