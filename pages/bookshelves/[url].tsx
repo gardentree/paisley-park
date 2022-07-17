b1d22ce7b0f09c82752c34bda286f43dda85d0fd
(ю:】 <gardentreex@gmail.com>
2022-07-17 18:20:54 +0900

    :beetle: Change bookshelf pages to CSR, because it uses local storage

--- pages/bookshelves/[url].tsx
+++ pages/bookshelves/[url].tsx
@@ -1,7 +1,9 @@
 import type {NextPage} from "next";
 import type React from "react";
 import {useRouter} from "next/router";
-import BookshelfContainer from "@/components/BookshelfContainer";
+
+import dynamic from "next/dynamic";
+const BookshelfContainer = dynamic(() => import("@/components/BookshelfContainer"), {ssr: false});
 
 const Bookshelves: NextPage = () => {
   const router = useRouter();
